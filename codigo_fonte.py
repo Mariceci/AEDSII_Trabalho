7c8a355313bc6e64f1e431f282293f03975820e7
Mariceci <ceci.maricruz@gmail.com>
2021-04-06 19:08:53 -0300

    Update codigo_fonte.py

---
+++ codigo_fonte.py
@@ -0,0 +1,13 @@
+#1. Incluir um novo cliente na lista
+#2. Associar um novo projeto a um cliente
+#3. Imprimir a lista de clientes ordenados pelo nome
+#4. Imprimir a lista de projetos ordenados pelo nome
+#5. Imprimir a lista de clientes com seus respectivos projetos em ordem crescente de nomes de 
+#clientes e nomes de projetos
+#6. Informar total de valor a receber
+#7. Informar total de valor já recebido
+#8. Dado o código de um projeto mostrar os dados do cliente e projeto
+#9. Dado o código do cliente, mostrar os dados do cliente incluindo projeto associados.
+#10. Remover um projeto de um cliente a partir do seu código
+#11. Remover um cliente a partir de seu código
+#12. Listar clientes que ainda devem e quais projetos ainda não foram pagos
