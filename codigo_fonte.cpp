beb91180e120ecb410d46c7da27593162612fbec
Mariceci <ceci.maricruz@gmail.com>
2021-04-07 18:53:53 -0300

    Update codigo_fonte.cpp

--- codigo_fonte.cpp
+++ codigo_fonte.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 int main(){
-    int respMENU;
-    cout<<"MENU\nDigite o numero referente a acao escolhida \n 1.  Incluir um novo cliente na lista \n 2.  Incluir um novo projeto a um cliente \n 3.  Lista dos clientes \n 4.  Lista dos projetos \n 5.  Lista dos clientes com seus respectivos projetos \n 6.  Quantia a pagar por um cliente \n 7.  Quantia já paga por um cliente \n 8.  Pesquisar o projeto pelo codigo \n 9.  Pesquisar o cliente pelo codigo  \n 10. Remover um projeto de um cliente  \n 11. Remover um cliente \n 12. Ver lista de devedores"<<endl;
-    cin >> respMENU;
+    int respMenu;
+    for (;;){
+    cout<<" Digite o numero referente a acao escolhida \n 1.  Incluir um novo cliente na lista \n 2.  Incluir um novo projeto a um cliente \n 3.  Lista dos clientes \n 4.  Lista dos projetos \n 5.  Lista dos clientes com seus respectivos projetos \n 6.  Quantia a pagar por um cliente \n 7.  Quantia já paga por um cliente \n 8.  Pesquisar o projeto pelo codigo \n 9.  Pesquisar o cliente pelo codigo  \n 10. Remover um projeto de um cliente  \n 11. Remover um cliente \n 12. Ver lista de devedores";
+    cin >>respMenu;
+    if (respMenu==1){
+        cout<< "Teste";
+        system("pause");
+        }
+    if (respMenu==2){
+    	
+	}
+	if (respMenu==2){
+		
+	}
+	if (respMenu==3){
+		
+	}
+	if (respMenu==4){
+		
+	}
+    if (respMenu==0){
+        break;
+        }
 
+    }
 }
