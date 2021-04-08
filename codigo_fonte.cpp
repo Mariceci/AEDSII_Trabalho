83f6e71bb47806040b3d736fe52294e7b83dfa20
Mariceci <ceci.maricruz@gmail.com>
2021-04-08 15:43:15 -0300

    Update codigo_fonte.cpp

--- codigo_fonte.cpp
+++ codigo_fonte.cpp
@@ -17,7 +17,12 @@ int main(){
     cout<<" Digite o numero referente a acao escolhida \n 1.  Incluir um novo cliente na lista \n 2.  Incluir um novo projeto a um cliente \n 3.  Lista dos clientes \n 4.  Lista dos projetos \n 5.  Lista dos clientes com seus respectivos projetos \n 6.  Quantia a pagar por um cliente \n 7.  Quantia jÃ¡ paga por um cliente \n 8.  Pesquisar o projeto pelo codigo \n 9.  Pesquisar o cliente pelo codigo  \n 10. Remover um projeto de um cliente  \n 11. Remover um cliente \n 12. Ver lista de devedores";
     cin >>respMenu;
     if (respMenu==1){
-        cout<< "Teste";
+        cout<< "Digite o nome do cliente: ";
+        cin >>
+        cout<< "Digite o endereço do cliente: ";
+        cin >>
+        cout<< "Digite o telefone do cliente: ";
+        cin >>
         system("pause");
         }
     if (respMenu==2){
